921c3e65a0e3851c9b71210042e34ac9d3a76a4b
Naveen Gowri <naveen.gowri@aspiresys.com>
2024-02-06 09:55:32 +0530

    initial commit

---
+++ src/main/java/loginValidator/LoginVerify.java
@@ -0,0 +1,57 @@
+package loginValidator;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginVerify
+ */
+public class LoginVerify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginVerify() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String un=request.getParameter("t1");
+		String pw=request.getParameter("t2");
+		String username="Naveen";
+		String password="12345678";
+		
+		if(un.equals(username) && pw.equals(password))
+		{
+		RequestDispatcher rd= request.getRequestDispatcher("/Success");
+		rd.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher rd= request.getRequestDispatcher("/Failed");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
